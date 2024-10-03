1d3261d8d579a8844686df26389498dca103a986
Dimitri Tabatadze <dimitritabatadze@Dimitris-MacBook-Pro.local>
2024-10-03 15:28:20 +0400

    added item switching

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,60 @@
 <script setup>
 import 'qti3-item-player-vue3/dist/qti3Player.css'
 
-let qti3Player;
+let qti3Player = null;
 let item;
+
+let items = [
+    "/wind-qti3/i66cc20026976d353700bd50789a79d0a15c/qti.xml",
+    "/4-qti3/i66cc20a40c29a353700f3415bcaeb697a3a/qti.xml",
+];
+
+async function loadData() {
+    for (let i = 0; i < items.length; i++) {
+        let response = await fetch(items[i], { mode: "no-cors" });
+        let xml = await response.text();
+        items[i] = xml;
+    }
+    data_is_fetched = true;
+}
+
+loadData();
 
 async function handlePlayerReady(_qti3Player) {
     qti3Player = _qti3Player;
-
-    let response = await fetch(
-        "/wind-qti3/i66cc20026976d353700bd50789a79d0a15c/qti.xml",
-        {
-            mode: "no-cors"
-        }
-    )
-    console.log(response)
-    let xml = await response.text();
-
-    console.log(xml);
-
-    qti3Player.loadItemFromXml(xml, {
-        guid: "0000",
-        status: "interacting",
-        sessionControl: {
-            max_attempts: 0, // no limit
-            show_feedback: false,
-            validate_responses: false
-        },
-    })
 }
 
 function handleItemReady(_item) {
     item = _item;
     console.log('QTI 3 Item XML is loaded and rendered!  The latest "attempt" has officially begun.')
 }
+
+let current_item = 0;
+
+function switchToItem(index) {
+
+    if (index != current_item) {
+        current_item = index;
+
+        qti3Player.loadItemFromXml(items[current_item], {
+            guid: "" + current_item,
+            status: "interacting",
+            sessionControl: {
+                max_attempts: 0, // no limit
+                show_feedback: false,
+                validate_responses: false
+            },
+        });
+    }
+}
 </script>
 
 <template>
     <Qti3Player ref="qti3player" container-class="qti3-player-container" @notifyQti3PlayerReady="handlePlayerReady"
         @notifyQti3ItemReady="handleItemReady" />
+
+    <button @click="switchToItem(0)">1</button>
+    <button @click="switchToItem(1)">2</button>
 </template>
 
 <style scoped>
