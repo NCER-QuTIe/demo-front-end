e5887aff96e86b68de528bd783b8c732073c7aff
Dimitri Tabatadze <dimitritabatadze@Dimitris-MacBook-Pro.local>
2024-10-04 16:05:13 +0400

    improved styling, demo final

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@ let items = [
     "/wind-qti3/i66cc20026976d353700bd50789a79d0a15c/qti.xml",
     "/4-qti3/i66cc20a40c29a353700f3415bcaeb697a3a/qti.xml",
     "/ლონდონის_თვალი_-_2_1725965625-qti3/i66cc1fe0d77b63537008681fb048efbbf44/qti.xml",
+    "/wind-qti3/i66cc20026976d353700bd50789a79d0a15c/qti.xml",
+    "/4-qti3/i66cc20a40c29a353700f3415bcaeb697a3a/qti.xml",
+    "/ლონდონის_თვალი_-_2_1725965625-qti3/i66cc1fe0d77b63537008681fb048efbbf44/qti.xml",
 ];
 
 async function loadData() {
@@ -47,9 +50,11 @@ let current_item = ref(-1);
 
 function switchToItem(index) {
     if (index >= items.length) {
-        index = 0;
+        alert("ტესტი დასრულდა");
+        return;
     }
     if (index < 0) {
+        return;
         index = items.length - 1;
     }
     if (index != current_item.value) {
@@ -69,29 +74,48 @@ function switchToItem(index) {
 </script>
 
 <template>
-    <div id="player-container">
-        <Qti3Player
-            ref="qti3player"
-            container-class="qti3-player-container"
-            @notifyQti3PlayerReady="handlePlayerReady"
-            @notifyQti3ItemReady="handleItemReady"
-        />
-    </div>
+    <main>
+        <div id="player-container">
+            <Qti3Player
+                ref="qti3player"
+                container-class="qti3-player-container"
+                @notifyQti3PlayerReady="handlePlayerReady"
+                @notifyQti3ItemReady="handleItemReady"
+            />
+        </div>
 
-    <div id="buttons">
-        <button @click="switchToItem(current_item - 1)">{{ "<" }}</button>
-        {{ current_item + 1 }}
-        <button @click="switchToItem(current_item + 1)">{{ ">" }}</button>
-    </div>
+        <div id="buttons">
+            <button
+                @click="switchToItem(current_item - 1)"
+                :disabled="current_item == 0"
+            >
+                წინა
+            </button>
+            <span style="font-size: 1.3em">
+                {{ current_item + 1 }}
+            </span>
+            <button @click="switchToItem(current_item + 1)">
+                {{ current_item < items.length - 1 ? "შემდეგი" : "დასრულება" }}
+            </button>
+        </div>
+    </main>
 </template>
 
 <style scoped>
+main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+}
+
 #player-container {
-    max-height: calc(100vh - 3em);
+    max-height: calc(100vh - 5em);
     margin-bottom: 0.5em;
     overflow: scroll;
     border: 1px solid black;
     border-radius: 4px;
+    padding: 1em;
 }
 
 #buttons {
@@ -100,5 +124,19 @@ function switchToItem(index) {
     gap: 1em;
     align-items: baseline;
     justify-content: center;
+}
+
+button {
+    padding: 1em 2em;
+    font-size: 1em;
+    background-color: #4a0;
+    color: white;
+    border: 1px solid #4c1;
+    border-radius: 1em;
+}
+
+button:disabled {
+    background-color: #cfb;
+    border-color: #efd;
 }
 </style>
