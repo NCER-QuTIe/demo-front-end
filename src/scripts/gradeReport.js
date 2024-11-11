f79874ebc26b8bfc37c93298bf1d017b1f5f0c56
D-T-666 <tabatadzedima@protonmail.com>
2024-11-11 18:55:41 +0400

    added georgianize

--- src/scripts/gradeReport.js
+++ src/scripts/gradeReport.js
@@ -1,10 +1,21 @@
+function georgianize(s) {
+  const dictionary = {
+    numAttempts: "რამდენჯერ სცადა",
+    duration: "რა დრო დაუთმო",
+  };
+  if (dictionary[s]) {
+    return dictionary[s];
+  }
+  return s.replace("RESPONSE", "პასუხი").replace("_", " ");
+}
+
 export function compileGradeReport(states) {
   console.log(states);
   let report = [];
   let header = [];
   for (let i = 0; i < states.length; i++) {
     for (let elt of states[i].state.responseVariables) {
-      header.push(i + 1 + " " + elt.identifier);
+      header.push(i + 1 + " " + georgianize(elt.identifier));
       report.push(elt.value);
     }
   }
