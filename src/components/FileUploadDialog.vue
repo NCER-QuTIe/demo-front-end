cee95f1296800612c38f572add17f6c948e579e0
D-T-666 <tabatadzedima@protonmail.com>
2024-12-22 00:43:15 +0400

    fixed duplicate filters

--- src/components/FileUploadDialog.vue
+++ src/components/FileUploadDialog.vue
@@ -12,110 +12,113 @@ let description = ref("");
 import { blob2base64 } from "@/scripts/blob2base64";
 
 async function upload() {
-    const myFile = file.value;
-    const b64 = await blob2base64(myFile, myFile.type);
+  const myFile = file.value;
+  const b64 = await blob2base64(myFile, myFile.type);
 
-    const obj = {
-        name: name.value,
-        description: description.value,
-        packageBase64: b64,
-        tags: tags.value.map((e) => e.value),
-        status: 0,
-    };
+  const obj = {
+    name: name.value,
+    description: description.value,
+    packageBase64: b64,
+    tags: tags.value.map((e) => e.value),
+    status: 0,
+  };
 
-    await fetch(`${import.meta.env.VITE_API_ROUTE}/api/admin/qtitest`, {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(obj),
-    });
+  await fetch(`${import.meta.env.VITE_API_ROUTE}/api/admin/qtitest`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(obj),
+  });
 
-    emit("close");
+  emit("close");
 }
 
 const tag_options = ref(props.old_tag_options);
 watchEffect(() => {
-    tag_options.value = props.old_tag_options;
+  tag_options.value = props.old_tag_options;
 });
 const op = ref();
 const new_tag = ref();
 function add_new_tag(val) {
-    tag_options.value.push({ label: new_tag.value, value: new_tag.value });
-    op.value.hide();
+  let x = { label: new_tag.value, value: new_tag.value };
+  if (!tag_options.value.includes(x)) {
+    tag_options.value.push(x);
+  }
+  op.value.hide();
 }
 </script>
 
 <template>
-    <Fluid class="w-[20rem]">
-        <div
-            class="p-4 rounded-border border border-surface flex flex-col gap-4 bg-surface"
-        >
-            <h1 class="text-lg font-bold text-center">ტესტის ატვირთვა</h1>
-            <FileUpload
-                ref="fileupload"
-                mode="basic"
-                name="demo[]"
-                customUpload
-                :maxFileSize="1000000"
-                @select="file = $event.files[0]"
-                chooseLabel="ფაილის არჩევა"
+  <Fluid class="w-[20rem]">
+    <div
+      class="p-4 rounded-border border border-surface flex flex-col gap-4 bg-surface"
+    >
+      <h1 class="text-lg font-bold text-center">ტესტის ატვირთვა</h1>
+      <FileUpload
+        ref="fileupload"
+        mode="basic"
+        name="demo[]"
+        customUpload
+        :maxFileSize="1000000"
+        @select="file = $event.files[0]"
+        chooseLabel="ფაილის არჩევა"
+      />
+
+      <FloatLabel variant="on">
+        <InputText id="test-name-input" v-model="name" />
+        <label for="test-name-input">ტესტის სახელი</label>
+      </FloatLabel>
+
+      <MultiSelect
+        v-model="tags"
+        display="chip"
+        placeholder="თაგები"
+        :options="tag_options"
+        optionLabel="label"
+        filter
+        :maxSelectedLabels="10"
+        class="w-full"
+      >
+        <template #footer>
+          <div class="p-3 flex justify-between">
+            <Button
+              label="Add New"
+              severity="secondary"
+              text
+              size="small"
+              icon="pi pi-plus"
+              @click="op.toggle"
             />
+          </div>
+        </template>
+      </MultiSelect>
 
-            <FloatLabel variant="on">
-                <InputText id="test-name-input" v-model="name" />
-                <label for="test-name-input">ტესტის სახელი</label>
-            </FloatLabel>
+      <FloatLabel variant="on">
+        <Textarea
+          id="test-description-textarea"
+          v-model="description"
+          autoResize
+          rows="5"
+          cols="30"
+        />
+        <label for="test-description-textarea">აღწერა</label>
+      </FloatLabel>
 
-            <MultiSelect
-                v-model="tags"
-                display="chip"
-                placeholder="თაგები"
-                :options="tag_options"
-                optionLabel="label"
-                filter
-                :maxSelectedLabels="10"
-                class="w-full"
-            >
-                <template #footer>
-                    <div class="p-3 flex justify-between">
-                        <Button
-                            label="Add New"
-                            severity="secondary"
-                            text
-                            size="small"
-                            icon="pi pi-plus"
-                            @click="op.toggle"
-                        />
-                    </div>
-                </template>
-            </MultiSelect>
+      <Button label="ატვირთვა" @click="upload()" />
+    </div>
+  </Fluid>
 
-            <FloatLabel variant="on">
-                <Textarea
-                    id="test-description-textarea"
-                    v-model="description"
-                    autoResize
-                    rows="5"
-                    cols="30"
-                />
-                <label for="test-description-textarea">აღწერა</label>
-            </FloatLabel>
-
-            <Button label="ატვირთვა" @click="upload()" />
-        </div>
-    </Fluid>
-
-    <Popover ref="op">
-        <div class="flex flex-col gap-4">
-            <InputText v-model="new_tag" />
-            <Button label="confirm" @click="add_new_tag" />
-        </div>
-    </Popover>
+  <Popover ref="op">
+    <div class="flex flex-col gap-4">
+      <InputText v-model="new_tag" />
+      <Button label="confirm" @click="add_new_tag" />
+    </div>
+  </Popover>
 </template>
 
 <style scoped>
 input[type="file"] {
-    display: none;
+  display: none;
 }
 </style>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -7,91 +7,93 @@ import { reactive, ref, watchEffect } from "vue";
 let data = reactive([]);
 
 fetch(`${import.meta.env.VITE_API_ROUTE}/api/admin/qtiTests`)
-    .then((data) => data.json())
-    .then((json) => {
-        data.push(...json);
-        console.log(data);
-    });
+  .then((data) => data.json())
+  .then((json) => {
+    data.push(...json);
+    console.log(data);
+  });
 
 async function deleteTest(ind) {
-    let res = await fetch(
-        `${import.meta.env.VITE_API_ROUTE}/api/admin/qtitest/${data[ind].id}`,
-        {
-            method: "DELETE",
-        },
-    );
-    if (res.ok) {
-        data.splice(ind, 1);
-    } else {
-        console.log(res);
-    }
+  let res = await fetch(
+    `${import.meta.env.VITE_API_ROUTE}/api/admin/qtitest/${data[ind].id}`,
+    {
+      method: "DELETE",
+    },
+  );
+  if (res.ok) {
+    data.splice(ind, 1);
+  } else {
+    console.log(res);
+  }
 }
 
 async function toggleStatus(ind) {
-    let res = await fetch(
-        `${import.meta.env.VITE_API_ROUTE}/api/admin/qtitest/status`,
-        {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                id: data[ind].id,
-                status: 1 - data[ind].status,
-            }),
-        },
-    );
+  let res = await fetch(
+    `${import.meta.env.VITE_API_ROUTE}/api/admin/qtitest/status`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: data[ind].id,
+        status: 1 - data[ind].status,
+      }),
+    },
+  );
 
-    if (res.ok) {
-        data[ind].status = 1 - data[ind].status;
-    } else {
-        console.log(res);
-    }
+  if (res.ok) {
+    data[ind].status = 1 - data[ind].status;
+  } else {
+    console.log(res);
+  }
 }
 
 let show_upload = ref(false);
 
 async function closeUpload() {
-    let res = await fetch(
-        `${import.meta.env.VITE_API_ROUTE}/api/admin/qtiTests`,
-    );
-    let json = await res.json();
+  let res = await fetch(`${import.meta.env.VITE_API_ROUTE}/api/admin/qtiTests`);
+  let json = await res.json();
 
-    data.splice(0);
-    data.push(...json);
-    show_upload.value = !show_upload.value;
+  data.splice(0);
+  data.push(...json);
+  show_upload.value = !show_upload.value;
 }
 let filter_tags = ref([]);
 
 const tag_options = ref([]);
 watchEffect(() => {
-    tag_options.value = data
-        .map((e) => e.tags)
-        .flat()
-        .map((e) => ({ label: e, value: e }));
+  let res = [];
+  for (let tags of data.map((e) => e.tags)) {
+    for (let tag of tags) {
+      if (!res.includes(tag)) {
+        console.log(tag);
+        res.push(tag);
+      }
+    }
+  }
+  tag_options.value = res.map((e) => ({ label: e, value: e }));
 });
 </script>
 
 <template>
-    <div class="grid grid-cols-[1fr_2fr_1fr] gap-4 h-full p-4">
-        <ListFilters :tag_options="tag_options" v-model="filter_tags" />
-        <TestList
-            :data="
-                data.filter((test) =>
-                    filter_tags.length === 0
-                        ? true
-                        : filter_tags.every((tag) =>
-                              test.tags.includes(tag.value),
-                          ),
-                )
-            "
-            @deleteTest="deleteTest"
-            @toggleStatus="toggleStatus"
-        />
-        <FileUploadDialog
-            :show="show_upload"
-            :old_tag_options="tag_options"
-            @close="closeUpload()"
-        />
-    </div>
+  <div class="grid grid-cols-[1fr_2fr_1fr] gap-4 h-full p-4">
+    <ListFilters :tag_options="tag_options" v-model="filter_tags" />
+    <TestList
+      :data="
+        data.filter((test) =>
+          filter_tags.length === 0
+            ? true
+            : filter_tags.every((tag) => test.tags.includes(tag.value)),
+        )
+      "
+      @deleteTest="deleteTest"
+      @toggleStatus="toggleStatus"
+    />
+    <FileUploadDialog
+      :show="show_upload"
+      :old_tag_options="tag_options"
+      @close="closeUpload()"
+    />
+  </div>
 </template>
