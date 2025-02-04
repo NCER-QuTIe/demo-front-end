24a61337bc491dd4b2c550f0108cf5b65372b9e7
D-T-666 <tabatadzedima@protonmail.com>
2025-02-04 12:29:16 +0400

    change the order and then name of search-by-title

--- src/components/ListFilters.vue
+++ src/components/ListFilters.vue
@@ -11,16 +11,16 @@ const searchTerm = defineModel("searchTerm");
 <template>
   <Fluid>
     <Panel header="ძებნა" class="rounded-border border border-surface" pt:content:class="flex flex-col gap-4">
-      <FloatLabel variant="on">
-        <label for="seach-keyowrd">სათაური</label>
-        <InputText id="seach-keyowrd" type="text" v-model="searchTerm" />
-      </FloatLabel>
-
       <template v-for="category in tagCategories">
         <UploadTagSelection v-model="filters[category]" :colors="tagColors[category]" :placeholder="tagLabels[category]"
           :options="tag_options[category]
             " />
       </template>
+
+      <FloatLabel variant="on">
+        <label for="seach-keyowrd">დასახელება</label>
+        <InputText id="seach-keyowrd" type="text" v-model="searchTerm" />
+      </FloatLabel>
     </Panel>
   </Fluid>
 </template>
